9f271ce48d119ecbb2369b09433fc94e630ba8aa
Platsax <platsaxn@icloud.se>
2017-11-17 13:25:48 +0100

    yee

--- ovning/JQuery/jQuery1.js
+++ ovning/JQuery/jQuery1.js
@@ -1,5 +1,19 @@
 $(function(){
-    $("#openinfo").click(function(){
+    $("body").click(function() {
+        var x = Math.floor(Math.random()*800);
+        var y = Math.floor(Math.random()*500);
+        $("#fly1").css({left:x,top:y});
+    });
+    $("#openinfo").click(function() { 
         $("#info").toggle();
+    });
+    $("#bgcolor").change(function(){
+        $("body").css("background-color", $(this).val());
+    });
+    $("#fcolor").change(function(){
+        $("body").css("color",$(this).val());
+    });
+    $("#rubrik").focusout(function(){
+        $("h1").text($(this).val());
     });
 });
