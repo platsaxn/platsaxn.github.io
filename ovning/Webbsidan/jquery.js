39bef3994dd94249ef081b9170e3b85cb8c753f8
Platsax <platsaxn@icloud.se>
2017-12-15 13:39:16 +0100

    Slutprojekt

--- ovning/Webbsidan/jquery.js
+++ ovning/Webbsidan/jquery.js
@@ -1,3 +1,24 @@
-$("#openinfo").click(function() { 
+$(function(){
+    $("#openinfo").click(function() { 
         $("#info").toggle();
+        $("#openinfo").toggle();
+        $("#closeinfo").toggle();
     });
+    $("#closeinfo").click(function() {
+        $("#info").toggle();
+        $("#openinfo").toggle();
+        $("#closeinfo").toggle();
+    });
+    $("#sakert").click(function() {
+        $("#sakert").css("border-bottom", "2px solid red")
+        $("#sakert").css("color", "red")
+    });
+    $("#schysst").click(function() {
+        $("#schysst").css("border-bottom","2px solid red")
+        $("#schysst").css("color", "red")
+    });
+    $("#standard").click(function() {
+        $("#standard").css("border-bottom","2px solid red")
+        $("#standard").css("color", "red")
+    });
+});
